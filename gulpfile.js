7fd01c6bd15d8246045eca28fbee97cc1bef1413
itsaso <itsaso.altuna@gmail.com>
2016-03-16 16:43:37 +0100

    gulp update img

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,11 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var imagemin = require('imagemin');
-
-var beep = require('beepbeep');
-var colors = require('colors');
-
 var browserSync = require('browser-sync').create();
 
+//Mensaje de error en terminal
+var beep = require('beepbeep');
+var colors = require('colors');
 
 var onError = function(err) {
   beep([200, 200]);
@@ -21,6 +20,7 @@ var onError = function(err) {
   this.emit('end');
 };
 
+//Convertir el SCSS a CSS
 gulp.task('css',function() {
 	return gulp.src('./src/scss/main.scss')
 	.pipe(plumber({
@@ -36,19 +36,27 @@ gulp.task('css',function() {
 	.pipe(gulp.dest('./dist/css'));
 });
 
+//Copiar el HTML de SRC a DIST
 gulp.task('html', function() {
     return gulp.src('./src/*.html')
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['css','html'], function() {
+//Copiar las IMG de SRC a DIST
+gulp.task('imagemin', function() {
+    return gulp.src('./src/img/*.png')
+    .pipe(gulp.dest('./dist/img'));
+});
+
+//Tarea por defecto GULP que muestra los cambios en tiempo real
+gulp.task('default', ['css','html','imagemin'], function() {
     browserSync.init({
         server: {
             baseDir: "./dist"
         },
         files: ['./dist/css/main.css']
     });
-    gulp.watch('./src/scss/**/*.scss', ['css']);
+    gulp.watch('./src/scss/*.png', ['imagemin']);
     gulp.watch('./src/*.html', ['html']);
     gulp.watch('./dist/*.html').on('change',browserSync.reload);
 });
