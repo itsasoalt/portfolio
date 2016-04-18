49c4abad4d05983b1c2cdf311af6b45797b12a5f
itsaso <itsaso.altuna@gmail.com>
2016-04-18 12:14:21 +0200

    menu changed plus index content

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-        $( ".header_menu--btn" ).on( "click", function() {
-          $(this).toggleClass( "closed" );
-          $('.header_menu--mobile').toggleClass( "closed" );
-        });
+	$( ".header_menu--btn" ).on( "click", function() {
+		$(this).toggleClass( "closed" );
+		$('.header_menu--mobile').toggleClass( "closed" );
+	});
 
-        
-  		});
+});
