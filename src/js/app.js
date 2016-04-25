163771f32ed211fbdba65447fc77bf962ab5fd8c
itsaso <itsaso.altuna@gmail.com>
2016-04-25 06:13:12 +0200

    portfolio main page almost done

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$( ".header_menu--btn" ).on( "click", function() {
 		$(this).toggleClass( "closed" );
 		$('.header_menu--mobile').toggleClass( "closed" );
+		$('.h1_message').toggleClass("h1_message--color");
+		$('.h1').toggleClass("h1--color");
 	});
 
 });
