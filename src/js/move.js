6b12b48c3564656177fcffb3c6cccd2c06f8d699
itsaso <itsaso.altuna@gmail.com>
2016-04-30 17:36:48 +0200

    js contact changes

--- src/js/move.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function() {
-var medida = $('.section_container--blockquote').position();
-
-	if ($(window).width()>=1024) {
-		$('.contact_img--controler').position(medida.top);
-	}
-});
